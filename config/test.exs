d24fcf958c68392cb23fafed9c0986a7f7f8cc68
Johannes Laamann <jelaamann@gmail.com>
2025-06-21 02:19:11 +0300

    Fix postgres port

--- config/test.exs
+++ config/test.exs
@@ -9,7 +9,6 @@ config :kawa, Kawa.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
-  port: 5433,
   database: "kawa_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
